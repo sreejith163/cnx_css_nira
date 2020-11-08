d21cdf80f81562996219c9b2a7f9340cadfc4ef1
Sreejith <SSreedharan@acr.org>
2020-11-08 11:16:43 +0530

    bug fix

--- Apps/Css.Web.UI/ClientApp/src/environments/environment.prod.ts
+++ Apps/Css.Web.UI/ClientApp/src/environments/environment.prod.ts
@@ -4,6 +4,9 @@ export const environment = {
     authBaseUrl: 'https://auth-api.concentrix.com/employee/authenticate?attributes[0]=mail&attributes[1]=uid&attributes[2]=displayname&attributes[3]=employeeid&client=',
     authAppToken: 'FGxyYfqQ3ht2omL0ARj4bySSa1rgrOeciUf'
   },
+  services: {
+    schedulingService: 'http://10.87.221.6:4202/api/v1'
+  },
   settings: {
     applicationUrl: 'http://10.87.222.56:4200',
     sessionName: 'session',
