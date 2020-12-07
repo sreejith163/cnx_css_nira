b08fe253b12880c01f196ed25cb17dd1411da429
Sreejith <SSreedharan@acr.org>
2020-12-07 13:00:57 +0530

    bug fix

--- Apps/Css.Web.UI/ClientApp/src/app/shared/services/generic-state-manager.service.ts
+++ Apps/Css.Web.UI/ClientApp/src/app/shared/services/generic-state-manager.service.ts
@@ -1,6 +1,18 @@
 import { Injectable } from '@angular/core';
+import { KeyValue } from '../models/key-value.model';
+
 @Injectable()
 export class GenericStateManagerService {
 
-  constructor() {}
+  language: KeyValue;
+
+  constructor() { }
+
+  getCurrentLanguage() {
+    return this.language;
+  }
+
+  setCurrentLanguage(language: KeyValue) {
+    this.language = language;
+  }
 }
