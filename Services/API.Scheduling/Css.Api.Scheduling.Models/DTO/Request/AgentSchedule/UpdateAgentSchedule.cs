662922c69457ce70e810bb1141ee79ebd6374209
Sreejith <SSreedharan@acr.org>
2020-12-11 17:39:02 +0530

    added validator

--- Services/API.Scheduling/Css.Api.Scheduling.Models/DTO/Request/AgentSchedule/UpdateAgentSchedule.cs
+++ Services/API.Scheduling/Css.Api.Scheduling.Models/DTO/Request/AgentSchedule/UpdateAgentSchedule.cs
@@ -1,5 +1,6 @@
 ﻿using Css.Api.Scheduling.Models.Domain;
 using Css.Api.Scheduling.Models.Enums;
+using System;
 using System.Collections.Generic;
 
 namespace Css.Api.Scheduling.Models.DTO.Request.AgentAdmin
@@ -10,6 +11,21 @@ namespace Css.Api.Scheduling.Models.DTO.Request.AgentAdmin
         /// Gets or sets the status.
         /// </summary>
         public SchedulingStatus Status { get; set; }
+
+        /// <summary>
+        /// Gets or sets the type of the agent schedule.
+        /// </summary>
+        public AgentScheduleType AgentScheduleType { get; set; }
+
+        /// <summary>
+        /// Gets or sets the date from.
+        /// </summary>
+        public DateTimeOffset? DateFrom { get; set; }
+
+        /// <summary>
+        /// Gets or sets the date to.
+        /// </summary>
+        public DateTimeOffset? DateTo { get; set; }
 
         /// <summary>
         /// Gets or sets the charts.
---
+++ Services/API.Scheduling/Css.Api.Scheduling/Validators/AgentSchedule/UpdateAgentScheduleValidator.cs
@@ -0,0 +1,21 @@
+﻿using Css.Api.Scheduling.Models.DTO.Request.AgentAdmin;
+using FluentValidation;
+
+namespace Css.Api.Css.Api.Scheduling.Validators.AgentSchedule
+{
+    /// <summary>
+    /// Validator for handling the validation of add AgentCategory object
+    /// </summary>
+    public class UpdateAgentScheduleValidator : AbstractValidator<UpdateAgentSchedule>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UpdateAgentScheduleValidator"/> class.
+        /// </summary>
+        public UpdateAgentScheduleValidator()
+        {
+            RuleFor(x => x.Status).NotEmpty();
+            RuleFor(x => x.ModifiedBy).NotEmpty();
+            RuleFor(x => x.AgentScheduleType).IsInEnum();
+        }
+    }
+}
