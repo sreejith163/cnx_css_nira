857f306a775d9c60eb494704dd5cf9694ee2f36c
Sreejith <SSreedharan@acr.org>
2021-01-06 09:54:19 +0530

    fixed ts lint

--- Apps/Css.Web.UI/ClientApp/src/app/shared/models/language-value.model.ts
+++ Apps/Css.Web.UI/ClientApp/src/app/shared/models/language-value.model.ts
@@ -1,18 +1,18 @@
 export const CSS_LANGUAGES = [
-  {code: 'en', name: 'English', sidebarLength: 'normal'},
-  {code: 'zh-CHS', name: 'Chinese (Simplified)', sidebarLength: 'normal'},
-  {code: 'zh-CHT', name: 'Chinese (Traditional)', sidebarLength: 'normal'},
-  {code: 'nl', name: 'Dutch', sidebarLength: 'normal'},
-  {code: 'de', name: 'Deutsch', length: 'normal'},
-  {code: 'fr', name: 'French', sidebarLength: 'long'},
-  {code: 'it', name: 'Italian', sidebarLength: 'long'},
-  {code: 'ja', name: 'Japanese', sidebarLength: 'long'},
-  {code: 'ru', name: 'Russian', sidebarLength: 'long'},
-  {code: 'es', name: 'Spanish', sidebarLength: 'long'},
+  { code: 'en', name: 'English', sidebarLength: 'normal' },
+  { code: 'zh-CHS', name: 'Chinese (Simplified)', sidebarLength: 'normal' },
+  { code: 'zh-CHT', name: 'Chinese (Traditional)', sidebarLength: 'normal' },
+  { code: 'nl', name: 'Dutch', sidebarLength: 'normal' },
+  { code: 'de', name: 'Deutsch', length: 'normal' },
+  { code: 'fr', name: 'French', sidebarLength: 'long' },
+  { code: 'it', name: 'Italian', sidebarLength: 'long' },
+  { code: 'ja', name: 'Japanese', sidebarLength: 'long' },
+  { code: 'ru', name: 'Russian', sidebarLength: 'long' },
+  { code: 'es', name: 'Spanish', sidebarLength: 'long' },
 ];
 
-export class Language{
-    code: string;
-    name: string;
-    sidebarLength?: string;
-  }
+export class Language {
+  code: string;
+  name: string;
+  sidebarLength?: string;
+}
