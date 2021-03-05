c96d2970d3d508cf059e296e87253fe0cc7ef954
Sreejith <SSreedharan@acr.org>
2021-03-05 14:42:25 +0530

    Bug fix

--- Apps/Css.Web.UI/ClientApp/src/app/modules/home/modules/scheduling-menu/scheduling-menu.module.ts
+++ Apps/Css.Web.UI/ClientApp/src/app/modules/home/modules/scheduling-menu/scheduling-menu.module.ts
@@ -6,35 +6,33 @@ import { SchedulingMenuRoutingModule } from './scheduling-menu-routing.module';
 import { AgentAdminListComponent } from './components/agent-admin/agent-admin-list/agent-admin-list.component';
 import { AddAgentProfileComponent } from './components/agent-admin/add-agent-profile/add-agent-profile.component';
 import { EditAgentProfileComponent } from './components/agent-admin/edit-agent-profile/edit-agent-profile.component';
-
-import { HorizontalScrollPipe } from './pipes/horizontal-scroll.pipe';
-
-import { AgentAdminService } from './services/agent-admin.service';
-import { AgentSchedulesService } from './services/agent-schedules.service';
-import { AgentScheduleManagersService } from './services/agent-schedule-managers.service';
 import { SchedulingGridComponent } from './components/scheduling-grid/scheduling-grid.component';
 import { ImportScheduleComponent } from './components/shared/import-schedule/import-schedule.component';
 import { CopyScheduleComponent } from './components/shared/copy-schedule/copy-schedule.component';
 import { MoveAgentsComponent } from './components/move-agents/move-agents/move-agents.component';
-
 import { SchedulingFilterComponent } from './components/shared/scheduling-filter/scheduling-filter.component';
-
-// for optimization and code cleaning
 import { ForecastScreenListComponent } from './components/forecast-screen/forecast-screen-list/forecast-screen-list.component';
-
 import { ActivityLogsScheduleComponent } from './components/shared/activity-logs-schedule/activity-logs-schedule.component';
 import { ViewOuScreenComponent } from './components/view-ou-screen/view-ou-screen.component';
-
-
-import { EntityHierarchyService } from './services/entity-hierarchy.service';
-import { EntityHierarchyComponent } from './components/entity-hierarchy/entity-hierarchy.component';
-import { NumericDirective } from 'src/app/shared/directives/numeric.directive';
-import { ActivityLogsService } from './services/activity-logs.service';
 import { SchedulingManagerComponent } from './components/scheduling-manager/scheduling-manager.component';
 import { ActivityLogsComponent } from './components/agent-admin/activity-logs/activity-logs.component';
-import { ContenteditableValueAccessor } from 'src/app/shared/directives/contenteditable.directive';
-import { BypassHtmlPipe } from 'src/app/shared/directives/bypassHtml.pipe';
 import { DateRangePopUpComponent } from './components/shared/date-range-pop-up/date-range-pop-up.component';
+import { MoveAgentsListComponent } from './components/move-agents/move-agents-list/move-agents-list.component';
+import { MoveAgentsSchedulingGroupTypeaheadComponent } from './components/move-agents/move-agents-scheduling-group-typeahead/move-agents-scheduling-group-typeahead.component';
+import { EntityHierarchyComponent } from './components/entity-hierarchy/entity-hierarchy.component';
+
+import { AgentAdminService } from './services/agent-admin.service';
+import { AgentSchedulesService } from './services/agent-schedules.service';
+import { AgentScheduleManagersService } from './services/agent-schedule-managers.service';
+import { EntityHierarchyService } from './services/entity-hierarchy.service';
+import { ActivityLogsService } from './services/activity-logs.service';
+import { MoveAgentsService } from './services/move-agents.service';
+
+import { NumericDirective } from 'src/app/shared/directives/numeric.directive';
+import { ContenteditableValueAccessor } from 'src/app/shared/directives/contenteditable.directive';
+
+import { HorizontalScrollPipe } from './pipes/horizontal-scroll.pipe';
+import { BypassHtmlPipe } from 'src/app/shared/directives/bypassHtml.pipe';
 
 const modules = [DragDropModule, SharedModule, SchedulingMenuRoutingModule];
 const components =
@@ -55,10 +53,13 @@ const components =
     ViewOuScreenComponent,
     NumericDirective,
     ActivityLogsComponent,
-    ContenteditableValueAccessor, 
+    MoveAgentsListComponent,
+    MoveAgentsSchedulingGroupTypeaheadComponent,
+    ContenteditableValueAccessor,
     BypassHtmlPipe
   ];
-const providers = [AgentAdminService, AgentSchedulesService, ActivityLogsService, EntityHierarchyService, AgentScheduleManagersService];
+const providers = [AgentAdminService, AgentSchedulesService, ActivityLogsService, EntityHierarchyService, AgentScheduleManagersService,
+                   MoveAgentsService];
 
 @NgModule({
   declarations: components,
