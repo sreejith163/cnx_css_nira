b41c9ee367c341d28cf48ead952932777b5364e2
Sreejith <SSreedharan@acr.org>
2021-03-06 13:32:33 +0530

    bug fix

--- Apps/Css.Web.UI/ClientApp/src/app/modules/home/modules/scheduling-menu/services/agent-schedule-managers.service.ts
+++ Apps/Css.Web.UI/ClientApp/src/app/modules/home/modules/scheduling-menu/services/agent-schedule-managers.service.ts
@@ -30,13 +30,6 @@ export class AgentScheduleManagersService extends HttpBaseService {
     }).pipe(catchError(this.handleError));
   }
 
-  // getAgentScheduleManager(agentScheduleManagerId: string) {
-  //   const url = `${this.baseURL}/AgentScheduleManagers/${agentScheduleManagerId}`;
-
-  //   return this.http.get<AgentChartResponse>(url)
-  //   .pipe(catchError(this.handleError));
-  // }
-
   updateScheduleManagerChart(updateAgentScheduleMangagerChart: UpdateAgentScheduleMangersChart) {
     const url = `${this.baseURL}/AgentScheduleManagers/charts`;
 
