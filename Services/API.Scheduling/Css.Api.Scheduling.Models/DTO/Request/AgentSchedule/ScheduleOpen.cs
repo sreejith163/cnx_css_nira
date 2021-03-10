08de96e8aa62f60f887dfc072b2ee77ea1d61ad6
Sreejith <SSreedharan@acr.org>
2021-03-10 11:08:13 +0530

    added missed file

---
+++ Services/API.Scheduling/Css.Api.Scheduling.Models/DTO/Request/AgentSchedule/ScheduleOpen.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Css.Api.Scheduling.Models.DTO.Request.AgentSchedule
+{
+    public class ScheduleOpen
+    {
+        /// <summary>
+        /// Gets or sets the time.
+        /// </summary>
+        public TimeSpan Time { get; set; }
+
+        /// <summary>
+        /// Gets or sets the schedule open.
+        /// </summary>
+        public int scheduleOpen { get; set; } = 1;
+    }
+}
